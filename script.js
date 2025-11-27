0a6c98aecd30238d6d1c9b19ac76055bc4374137
CarlossMartinez <carkevinmartinez@gmail.com>
2025-11-27 16:33:22 +0100

    Remove mobile user alert from script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("spinner").style.display = "none";
-    alert("Esta web esta principalmente enfocada en usuarios de telefono movil, por lo que si la estas viendo desde un ordenador, te recomendamos que la veas desde el movil. Gracias.");
-});
 
